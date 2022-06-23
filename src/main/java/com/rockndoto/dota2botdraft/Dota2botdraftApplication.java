1a2c4209ba74950465ccf579739b13e9e37e90b0
Ashok Tamang <ashoktamang52@gmail.com>
2022-06-23 08:15:02 +0545

    feat: Init starter project.

---
+++ src/main/java/com/rockndoto/dota2botdraft/Dota2botdraftApplication.java
@@ -0,0 +1,13 @@
+package com.rockndoto.dota2botdraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dota2botdraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dota2botdraftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rockndoto/dota2botdraft/Dota2botdraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rockndoto.dota2botdraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dota2botdraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
